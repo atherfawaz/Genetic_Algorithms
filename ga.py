75357641ee8d6f2bcb71cdb72d3dd62018a200c0
atherfawaz <atherfawaz@users.noreply.github.com>
2020-03-09 19:17:06 +0500

    Implemented an optimized fitness function. My Wi-Fi just went offline so we shall stop here

--- ga.py
+++ ga.py
@@ -10,10 +10,10 @@ ROWS = IMAGE.shape[0]
 COLUMNS = IMAGE.shape[1]
 DEPTH = IMAGE.shape[2]
 DIFFERENCE_MATRIX = numpy.zeros((ROWS, COLUMNS, DEPTH))
-GENETIC_IMAGE = numpy.zeros((ROWS, COLUMNS, DEPTH))
+GENETIC_IMAGE = numpy.random.randint(0, 255, size=(ROWS, COLUMNS, DEPTH))
 GENERATIONS = 1000
 UNFIT = []
-THRESHOLD = 0
+THRESHOLD = 0 # set to 0 for no mismatch between images
 # GLOBALS
 
 
@@ -30,12 +30,21 @@ class Coordinate:
         self.z = z
         self.difference = difference
 
+# Operates on a list
+def optimized_fitness():
+    global UNFIT
+    for var in UNFIT:
+        difference = IMAGE[var.x][var.y][var.z] - GENETIC_IMAGE[var.x][var.y][var.z]
+        var.difference = difference  # debtable if we should update the array here
+        if (abs(difference) < THRESHOLD):
+            # remove this element if it is at its correct value
+            UNFIT.remove(var)
 
+# O(n^3)
 def fitness_function():
     global UNFIT
     global DIFFERENCE_MATRIX
     UNFIT = []
-    # O(n^3)
     for i in range(0, ROWS):
         for j in range(0, COLUMNS):
             for k in range(0, DEPTH):
@@ -51,9 +60,11 @@ if __name__ == "__main__":
     # display image
     # plt.show()
     current_generation = 0
+    # call an O(n^3) loop once to set up the differences
+    fitness_function()
     while (current_generation < GENERATIONS):
         # gauge current picture
-        fitness_function()
+        optimized_fitness()
         # selection
         UNFIT.sort(key=lambda Coordinate: Coordinate.difference, reverse=True)
         # cross over
