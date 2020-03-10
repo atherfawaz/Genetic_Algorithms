56dc13dfc135a94dfe429e08e19bcdc40f9a44bf
atherfawaz <atherfawaz@users.noreply.github.com>
2020-03-10 15:37:28 +0500

    Working implementation

--- ga.py
+++ ga.py
@@ -9,19 +9,23 @@ import numpy
 from random import seed
 from random import randint, random
 import random
+from PIL import Image
+img = Image.open('imageB.bmp').convert('LA')
+img.save('greyscale.png')
 
 # GLOBALS
-FILE = 'new.png'
+FILE = 'imageB.bmp'
 IMAGE = mpimg.imread(FILE)
-IMAGE = cv2.resize(IMAGE,(20,28))
+plt.imshow(IMAGE)
+# IMAGE = cv2.resize(IMAGE,(20,28))
 # plt.imsave('original_compressed.png', IMAGE)
 ROWS = IMAGE.shape[0]
 COLUMNS = IMAGE.shape[1]
-#DEPTH = 1
+DEPTH = IMAGE.shape[2]
 POPULATION = []
 NEW_POPULATION = []
 POPULATION_SIZE = 100
-GENETIC_IMAGE = np.random.randint(0, 255, size=(ROWS, COLUMNS))
+GENETIC_IMAGE = np.random.randint(0, 255, size=(ROWS, COLUMNS, DEPTH))
 GENERATIONS = 20000
 UNFIT = []
 THRESHOLD = 10  # set to 0 for no mismatch between images
@@ -34,7 +38,7 @@ class Population_Class:
     difference = None
 
     def __init__(self, difference):
-        self.array = np.random.randint(0, 255, size=(ROWS, COLUMNS))
+        self.array = np.random.randint(0, 255, size=(ROWS, COLUMNS, DEPTH))
         self.difference = difference
 
 def fitness_function():
@@ -42,9 +46,9 @@ def fitness_function():
         var.difference = np.sum(np.abs(np.subtract(var.array, IMAGE))).astype(int)
 
 def cross_over(pic1, pic2):
-    child = Population_Class(None)    
-    child.array[0:int(ROWS/2)][0:int(COLUMNS)] = pic1.array[int(ROWS/2):ROWS][0:int(COLUMNS)]
-    child.array[int(ROWS/2) : ROWS][0:int(COLUMNS)] = pic2.array[0:int(ROWS/2)][0:int(COLUMNS)]
+    child = Population_Class(None)
+    child.array[0:int(ROWS/2)][:][:] = pic1.array[0:int(ROWS/2)][:][:]
+    child.array[int(ROWS/2) : ROWS][:][:] = pic2.array[int(ROWS/2):ROWS][:][:]
     return child
 
 def mutate(pic):
@@ -54,7 +58,7 @@ def mutate(pic):
         indexR = np.random.randint(0,ROWS)
         indexC = np.random.randint(0,COLUMNS)
         value = np.random.uniform(0, 255, 1)
-        pic.array[indexR][indexC] = value
+        pic.array[indexR][indexC][:] = value
     return pic
 
 def generate_population():
@@ -84,20 +88,24 @@ if __name__ == "__main__":
         if (current_generation % 100 == 0):
             print('Generation: ', current_generation)
             print("Cost: ", POPULATION[0].difference)
+            plt.imsave('temp.bmp', GENETIC_IMAGE.astype(np.uint8))
 
         # selection; selecting top 20%
         for i in range(0, round((10/100) * POPULATION_SIZE)):
             NEW_POPULATION.append(POPULATION[i])
 
-        # cross over; select 2 pictures from 60% of the fittest ones at random, then apply crossover to them
-        for i in range(0, round((90/100) * POPULATION_SIZE)):
+        # cross over; select 2 pictures from 50% of the fittest ones at random, 
+        # then apply crossover to them, store both parents and children in new pop
+        for i in range(0, round((30/100) * POPULATION_SIZE)):
             pic1 = random.choice(POPULATION[: int(POPULATION_SIZE/2)])
             pic2 = random.choice(POPULATION[: int(POPULATION_SIZE/2)])
+            NEW_POPULATION.append(pic1)
+            NEW_POPULATION.append(pic2)
             NEW_POPULATION.append(cross_over(pic1, pic2))
 
-        # mutation; select 1 picture from 50% of the fittest one at random, then mutate some of its pixels
+        # mutation; select 1 picture from all at random, then mutate some of its pixels
         for i in range(0, round((10/100) * POPULATION_SIZE)):
-            NEW_POPULATION.append(mutate(random.choice(POPULATION[: int(POPULATION_SIZE/2)])))
+            NEW_POPULATION.append(mutate(random.choice(POPULATION[:])))
 
         # increment generation
         POPULATION = NEW_POPULATION
